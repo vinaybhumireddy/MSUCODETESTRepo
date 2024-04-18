63e34b4d34325295cbe392314e08116c291c9abe
Vinay <53284302+vinaybhumireddy@users.noreply.github.com>
2024-04-18 14:59:55 -0400

    Initial commit
    Initial commit for code test

---
+++ MSUFRIBCodeTest/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MSUFRIBCodeTest.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string? MessageBody { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime SentDate { get; set; }
+    }
+}
---
+++ MSUFRIBCodeTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MSUFRIBCodeTest.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MSUFRIBCodeTestContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSUFRIBCodeTestContext") ?? throw new InvalidOperationException("Connection string 'MSUFRIBCodeTestContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
